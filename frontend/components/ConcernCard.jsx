15b764f5fced3f71d021bb5abdf4697c7fe46324
pradeep bolla <136964306+pradeep4129d@users.noreply.github.com>
2024-07-07 07:47:10 +0530

    Add files via upload

--- frontend/components/ConcernCard.jsx
+++ frontend/components/ConcernCard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ConcernCard = (props) => {
+  const [dropclass,setdropclass]=useState('')
   function arrayBufferToImage(buffer) {
     const blob = new Blob([new Uint8Array(buffer)], { type: 'image/png' });
     return URL.createObjectURL(blob);
@@ -10,11 +11,38 @@ export const ConcernCard = (props) => {
   return (
     <div className='concern-card'>
       <div className="image">
-            <div class="like-dislike-container">		
+            <div className="menu">
+              <button onClick={()=>{setdropclass(dropclass===''?'t':'')}} className="menu-icon"><ion-icon name="ellipsis-vertical"></ion-icon></button>
+              <button className={"editBtn "+dropclass}>
+                <svg height="1em" viewBox="0 0 512 512">
+                  <path
+                    d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                  ></path>
+                </svg>
+              </button>
+              <button class={"bin-button "+dropclass}>
+                  <svg class="bin-top"viewBox="0 0 39 7"fill="none"xmlns="http://www.w3.org/2000/svg">
+                    <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
+                    <line x1="12"  y1="1.5" x2="26.0357"y2="1.5"stroke="white" stroke-width="3"
+                    ></line>
+                  </svg>
+                  <svg class="bin-bottom"viewBox="0 0 33 39"fill="none"xmlns="http://www.w3.org/2000/svg" >
+                    <mask id="path-1-inside-1_8_19" fill="white">
+                      <path
+                        d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+                      ></path>
+                    </mask>
+                    <path d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"fill="white"mask="url(#path-1-inside-1_8_19)"></path>
+                    <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
+                    <path d="M21 6V29" stroke="white" stroke-width="4"></path>
+                  </svg>
+              </button>
+            </div>
+            <div class="like-dislike-container">
               <div class="icons-box">
                 <div class="icons">
                   <label class="btn-label" for="like-checkbox">
-                    <span class="like-text-content">123</span>
+                    <span class="like-text-content">00</span>
                     <input class="input-box" id="like-checkbox" type="checkbox"/>
                     <svg class="svgs" id="icon-like-solid" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"></path></svg>
                     <svg class="svgs" id="icon-like-regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M323.8 34.8c-38.2-10.9-78.1 11.2-89 49.4l-5.7 20c-3.7 13-10.4 25-19.5 35l-51.3 56.4c-8.9 9.8-8.2 25 1.6 33.9s25 8.2 33.9-1.6l51.3-56.4c14.1-15.5 24.4-34 30.1-54.1l5.7-20c3.6-12.7 16.9-20.1 29.7-16.5s20.1 16.9 16.5 29.7l-5.7 20c-5.7 19.9-14.7 38.7-26.6 55.5c-5.2 7.3-5.8 16.9-1.7 24.9s12.3 13 21.3 13L448 224c8.8 0 16 7.2 16 16c0 6.8-4.3 12.7-10.4 15c-7.4 2.8-13 9-14.9 16.7s.1 15.8 5.3 21.7c2.5 2.8 4 6.5 4 10.6c0 7.8-5.6 14.3-13 15.7c-8.2 1.6-15.1 7.3-18 15.1s-1.6 16.7 3.6 23.3c2.1 2.7 3.4 6.1 3.4 9.9c0 6.7-4.2 12.6-10.2 14.9c-11.5 4.5-17.7 16.9-14.4 28.8c.4 1.3 .6 2.8 .6 4.3c0 8.8-7.2 16-16 16H286.5c-12.6 0-25-3.7-35.5-10.7l-61.7-41.1c-11-7.4-25.9-4.4-33.3 6.7s-4.4 25.9 6.7 33.3l61.7 41.1c18.4 12.3 40 18.8 62.1 18.8H384c34.7 0 62.9-27.6 64-62c14.6-11.7 24-29.7 24-50c0-4.5-.5-8.8-1.3-13c15.4-11.7 25.3-30.2 25.3-51c0-6.5-1-12.8-2.8-18.7C504.8 273.7 512 257.7 512 240c0-35.3-28.6-64-64-64l-92.3 0c4.7-10.4 8.7-21.2 11.8-32.2l5.7-20c10.9-38.2-11.2-78.1-49.4-89zM32 192c-17.7 0-32 14.3-32 32V448c0 17.7 14.3 32 32 32H96c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32z"></path></svg>
@@ -35,9 +63,14 @@ export const ConcernCard = (props) => {
       </div>
       <div className="progress">
         <p>progress: {props.data.progress}</p>
+        <div className="global">
+        {
+            props.data.public?(<p>Public<ion-icon name="eye"></ion-icon></p>):(<p>Private<ion-icon name="eye-off"></ion-icon></p>)
+        }
+        </div>
       </div>
       <div className="location">
-        <p><ion-icon name="location-outline"></ion-icon> {props.data.location} </p>
+        <p><ion-icon name="location"></ion-icon> {props.data.location} </p>
         <div>block:{block[JSON.parse(props.data.block)-1]}</div>
       </div>
     </div>
